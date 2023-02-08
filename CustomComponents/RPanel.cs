679c5ce1efe8058dc950f396e87caa7b4c4aeab7
HainnTheGeek <maymyomoe90@hotmail.com>
2023-02-09 00:48:34 +0630

    Add project files.

---
+++ CustomComponents/RPanel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomComponents
+{
+    public class RPanel : Panel
+    {
+        //Fields
+        private int borderRadius = 30;
+        private float gradientAngle = 90F;
+        private Color gradientTopCOlor = Color.DodgerBlue;
+        private Color gradientBottomColor = Color.CadetBlue;
+
+        //DropShadow
+
+
+        //Constructor
+        public RPanel()
+        {
+            this.BackColor = Color.White;
+            this.ForeColor = Color.Black;
+            this.Size = new Size(Size.Width, Size.Height);
+        }
+
+        //properties
+        public int BorderRadius
+        { get => borderRadius; set { borderRadius = value; this.Invalidate(); } }
+        public float GradientAngle
+        { get => gradientAngle; set { gradientAngle = value; this.Invalidate(); } }
+        public Color GradientTopColor
+        { get => gradientTopCOlor; set { gradientTopCOlor = value; this.Invalidate(); } }
+        public Color GradientBottomColor
+        { get => gradientBottomColor; set { gradientBottomColor = value; this.Invalidate(); } }
+
+
+
+        //Methods
+        private GraphicsPath GetRPanelPath(RectangleF rectangle, float radius)
+        {
+            GraphicsPath graphicsPath = new GraphicsPath();
+            Bitmap shadowBmp = new Bitmap(this.Width, this.Height, PixelFormat.Format32bppArgb);
+            graphicsPath.StartFigure();
+            graphicsPath.AddArc(rectangle.Width - radius, rectangle.Height - radius, radius, radius, 0, 90);
+            graphicsPath.AddArc(rectangle.X, rectangle.Height - radius, radius, radius, 90, 90);
+            graphicsPath.AddArc(rectangle.X, rectangle.Y, radius, radius, 180, 90);
+            graphicsPath.AddArc(rectangle.Width - radius, rectangle.Y, radius, radius, 270, 90);
+            DrawShadowSmooth(graphicsPath, 100, 60, shadowBmp);
+            graphicsPath.CloseFigure();
+            return graphicsPath;
+        }
+
+        //Overridden Methods
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            //Gradient
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            LinearGradientBrush rBrush = new LinearGradientBrush(this.ClientRectangle, this.GradientTopColor, this.GradientBottomColor, this.gradientAngle);
+            Graphics rGraphics = e.Graphics;
+            rGraphics.FillRectangle(rBrush, ClientRectangle);
+
+
+            //BorderRadius
+            RectangleF rectangleF = new RectangleF(0, 0, this.Width, this.Height);
+            if (borderRadius > 2)
+            {
+                using (GraphicsPath graphicsPath = GetRPanelPath(rectangleF, borderRadius))
+                using (Pen pen = new Pen(this.Parent.BackColor, 2))
+                {
+                    this.Region = new Region(graphicsPath);
+                    e.Graphics.DrawPath(pen, graphicsPath);
+                }
+            }
+            else
+            {
+                this.Region = new Region(rectangleF);
+            }
+        
+        }
+
+        private static void DrawShadowSmooth(GraphicsPath gp, int intensity, int radius, Bitmap dest)
+        {
+            using (Graphics g = Graphics.FromImage(dest))
+            {
+                g.Clear(Color.Transparent);
+                g.CompositingMode = CompositingMode.SourceCopy;
+                double alpha = 0;
+                double astep = 0;
+                double astepstep = (double)intensity / radius / (radius / 2D);
+                for (int thickness = radius; thickness > 0; thickness--)
+                {
+                    using (Pen p = new Pen(Color.FromArgb((int)alpha, 0, 0, 0), thickness))
+                    {
+                        p.LineJoin = LineJoin.Round;
+                        g.DrawPath(p, gp);
+                    }
+                    alpha += astep;
+                    astep += astepstep;
+                }
+            }
+        }
+    }
+}
---
+++ RPanel/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace RPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rPanel1 = new CustomComponents.RPanel();
+            this.SuspendLayout();
+            // 
+            // rPanel1
+            // 
+            this.rPanel1.BackColor = System.Drawing.Color.White;
+            this.rPanel1.BorderRadius = 50;
+            this.rPanel1.ForeColor = System.Drawing.Color.Black;
+            this.rPanel1.GradientAngle = 45F;
+            this.rPanel1.GradientBottomColor = System.Drawing.Color.SkyBlue;
+            this.rPanel1.GradientTopColor = System.Drawing.Color.DodgerBlue;
+            this.rPanel1.Location = new System.Drawing.Point(546, 191);
+            this.rPanel1.Name = "rPanel1";
+            this.rPanel1.Size = new System.Drawing.Size(323, 164);
+            this.rPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 550);
+            this.Controls.Add(this.rPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomComponents.RPanel rPanel1;
+    }
+}
